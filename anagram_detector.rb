e0ca42c7bfcacb0f2a902f487319a1575465fc6c
Josh Meek <jmeek@covermymeds.com>
2016-06-14 10:33:01 -0400

    complete feature

--- anagram_detector.rb
+++ anagram_detector.rb
@@ -1,16 +1,16 @@
 # Implement this in such a way that when called below, detect_anagram will result in true or false.
 def canonical(word)
+  word.downcase.chars.sort.to_s
 end
 
 def detect_anagram(word1, word2)
   canonical(word1) == canonical(word2)
 end
 
-p "Do NOT change any code below this line"
+p 'Do NOT change any code below this line'
 p detect_anagram('angel', 'glean') == true
 p detect_anagram('glean', 'angel') == true
 p detect_anagram('pants', 'pants') == true
 p detect_anagram('CinEmA', 'iceman') == true
 p detect_anagram('defgh8', 'g8hefd') == true
-
-
+p detect_anagram('bird', 'house') == false
